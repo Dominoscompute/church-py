8fd7edf8660e0f3fe71bf3abfb28948aeff26e03
DominosCompute <106498930+Dominoscompute@users.noreply.github.com>
2022-08-07 23:32:39 -0600

    reformatted and added some comments

--- church.py
+++ church.py
@@ -2,8 +2,15 @@ def main():
     import lib
     def uncons(xs):
         return (xs[0],xs[1:])
+    # prints 0
     print(lib.ch2num(lib.zero))
+    
+    # prints 99
     print(lib.ch2num(lib.pred(lib.one_hundred)))
+    
+    # prints 69
     print(lib.ch2int(lib.smul(lib.ints(-3))(lib.ints(-23))))
+
+
 if __name__ == "__main__":
     main()
